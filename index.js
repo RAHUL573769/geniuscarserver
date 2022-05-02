f1b204c6717380eee837a4d98a0b3c65010c1d1d
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-02 09:45:00 +0600

    dataconnected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
-const { send } = require("express/lib/response");
+
 require("dotenv").config();
 
 const app = express();
@@ -11,14 +11,28 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h2c4c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
-async function run() {}
+async function run() {
+  try {
+    await client.connect();
+    console.log("db connected");
+    const serviceCollection = client.db("geniusCar").collection("service");
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
 run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Hi From genuise Car");
