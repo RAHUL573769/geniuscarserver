d88f264ef6e77698c09fe2f01699bb7cc20c647d
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-02 09:12:15 +0600

    envadded

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const { send } = require("express/lib/response");
 require("dotenv").config();
@@ -8,6 +9,19 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h2c4c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Hi From genuise Car");
