fd8a882eca2facb5870a67382f114d13d021e726
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-02 09:57:37 +0600

    newurlcreated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 
 require("dotenv").config();
@@ -28,6 +28,15 @@ async function run() {
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: ObjectId(id),
+      };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
     });
   } finally {
   }
